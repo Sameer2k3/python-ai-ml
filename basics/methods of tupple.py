5efe9283e44a826cd4a831c1aa812ca419b02465
sameer <bv014010@gmail.com>
2026-07-19 01:03:31 +0530

    initial commit

--- basics/methods of tupple.py
+++ basics/methods of tupple.py
@@ -2,9 +2,15 @@
 countries=("india","china","france")
 a=countries.count("india")
 print(a)
+
 temp=list(countries)
+
 temp.append("russia")  #add items
+
 temp.pop(2)   #remove items
+
 temp[2]="finland"    #change items
+
 countries=tuple(temp)
+
 print(countries)
---
+++ basics/packing-unoacking.py
@@ -0,0 +1,15 @@
+student=("sameer",9.8,22)
+
+# unpacking
+name,cgpa,age=student
+
+print(name)
+print(age)
+print(cgpa)
+
+species= "cat"
+sound="meow"
+legs=4
+# packing
+animal=species,sound,legs
+print(animal)
